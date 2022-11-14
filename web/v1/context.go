4bde1cc36577248140f511121f1519c8e777ce3a
xuqil <xuqi19218@163.com>
2022-11-14 20:10:12 +0800

    初始化 web 框架

--- web/v1/context.go
+++ web/v1/context.go
@@ -2,6 +2,7 @@ package web
 
 import "net/http"
 
+// Context 请求上下文
 type Context struct {
 	Req  *http.Request
 	Resp http.ResponseWriter
--- web/v3/context.go
+++
@@ -1,9 +0,0 @@
-package web
-
-import "net/http"
-
-type Context struct {
-	Req        *http.Request
-	Resp       http.ResponseWriter
-	PathParams map[string]string
-}
