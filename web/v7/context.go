388374dc026e20092d1792340a2a538eae3cdcaf
xuqil <xuqi19218@163.com>
2022-11-16 20:14:28 +0800

    route: 正则路由的注册

---
+++ web/v7/context.go
@@ -0,0 +1,12 @@
+//go:build v7
+
+package web
+
+import "net/http"
+
+// Context 请求上下文
+type Context struct {
+	Req        *http.Request
+	Resp       http.ResponseWriter
+	PathParams map[string]string
+}
--- web/v7/route_test.go
+++ web/v7/route_test.go
@@ -1,3 +1,5 @@
+//go:build v7
+
 package web
 
 import (
