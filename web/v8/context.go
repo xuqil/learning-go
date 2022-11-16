d94abb99a62a2b6b44f4dcc50884c1f84d5db828
xuqil <xuqi19218@163.com>
2022-11-16 22:28:48 +0800

    route: 正则路由匹配和优化

---
+++ web/v8/context.go
@@ -0,0 +1,10 @@
+package web
+
+import "net/http"
+
+// Context 请求上下文
+type Context struct {
+	Req        *http.Request
+	Resp       http.ResponseWriter
+	PathParams map[string]string
+}
---
+++ web/v8/server_e2e_test.go
@@ -0,0 +1,38 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	h := &HTTPServer{}
+
+	h.addRoute(http.MethodGet, "/user", func(ctx *Context) {
+		fmt.Println("处理第一件事")
+		fmt.Println("处理第二件事")
+	})
+	handler1 := func(ctx *Context) {
+		fmt.Println("处理第一件事")
+	}
+
+	handler2 := func(ctx *Context) {
+		fmt.Println("处理第二件事")
+	}
+
+	// 用户自己去管这种
+	h.addRoute(http.MethodGet, "/user/detail", func(ctx *Context) {
+		handler1(ctx)
+		handler2(ctx)
+	})
+
+	h.GET("/login", func(ctx *Context) {
+
+	})
+
+	err := h.Start(":8081")
+	if err != nil {
+		return
+	}
+}
