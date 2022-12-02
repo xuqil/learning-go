8cb02433dd13198e20d3cea6e8c92bf8be9f9e93
xuqil <xuqi19218@163.com>
2022-12-02 20:58:13 +0800

    orm: select

---
+++ orm/v1/select.go
@@ -0,0 +1,120 @@
+package orm
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type Selector[T any] struct {
+	table string
+	where []Predicate
+	sb    *strings.Builder
+	args  []any
+}
+
+func (s *Selector[T]) Build() (*Query, error) {
+	s.sb = &strings.Builder{}
+	sb := s.sb
+	sb.WriteString("SELECT * FROM ")
+	// 把表名拿到
+	if s.table == "" {
+		var t T
+		typ := reflect.TypeOf(t)
+		sb.WriteByte('`')
+		sb.WriteString(typ.Name())
+		sb.WriteByte('`')
+	} else {
+		//sb.WriteByte('`')
+		sb.WriteString(s.table)
+		//sb.WriteByte('`')
+	}
+	if len(s.where) > 0 {
+		sb.WriteString(" WHERE ")
+		p := s.where[0]
+		for i := 1; i < len(s.where); i++ {
+			p = p.And(s.where[i])
+		}
+		if err := s.buildExpression(p); err != nil {
+			return nil, err
+		}
+	}
+
+	sb.WriteByte(';')
+	return &Query{
+		SQL:  sb.String(),
+		Args: s.args,
+	}, nil
+}
+
+func (s *Selector[T]) buildExpression(expr Expression) error {
+	switch exp := expr.(type) {
+	case nil:
+	case Predicate:
+		// 在这里处理 p
+		// p.left 构建好
+		// p.opt 构建好
+		// p.right 构建好
+		_, ok := exp.left.(Predicate)
+		if ok {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.left); err != nil {
+			return err
+		}
+		if ok {
+			s.sb.WriteByte(')')
+		}
+		s.sb.WriteByte(' ')
+		s.sb.WriteString(exp.op.String())
+		s.sb.WriteByte(' ')
+		_, ok = exp.right.(Predicate)
+		if ok {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.right); err != nil {
+			return err
+		}
+		if ok {
+			s.sb.WriteByte(')')
+		}
+	case Column:
+		s.sb.WriteByte('`')
+		s.sb.WriteString(exp.name)
+		s.sb.WriteByte('`')
+	case value:
+		s.sb.WriteByte('?')
+		s.addArg(exp.val)
+	default:
+		return fmt.Errorf("orm: 不支持的表达式类型 %v", expr)
+	}
+	return nil
+}
+func (s *Selector[T]) addArg(val any) *Selector[T] {
+	if s.args == nil {
+		// 预估容量
+		s.args = make([]any, 0, 8)
+	}
+	s.args = append(s.args, val)
+	return s
+}
+func (s *Selector[T]) From(table string) *Selector[T] {
+	s.table = table
+	return s
+}
+
+func (s *Selector[T]) Where(ps ...Predicate) *Selector[T] {
+	s.where = ps
+	return s
+}
+
+func (s *Selector[T]) Get(ctx context.Context) (*T, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Selector[T]) GetMulti(ctx context.Context) ([]*T, error) {
+	//TODO implement me
+	panic("implement me")
+}
