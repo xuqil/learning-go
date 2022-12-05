c0787f4964ecb71fe910a36bafda82e9b068875b
xuqil <xuqi19218@163.com>
2022-12-05 22:09:22 +0800

    orm: 注册中心

---
+++ orm/v3/db.go
@@ -0,0 +1,26 @@
+package orm
+
+type DBOption func(db *DB)
+
+type DB struct {
+	r *registry
+}
+
+func NewDB(opts ...DBOption) (*DB, error) {
+	res := &DB{
+		r: newRegistry(),
+	}
+	for _, opt := range opts {
+		opt(res)
+	}
+	return res, nil
+}
+
+// MustNewDB 不提供 error
+func MustNewDB(opts ...DBOption) *DB {
+	db, err := NewDB(opts...)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
