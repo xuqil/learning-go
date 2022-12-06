6521c7f2b85c4e8d227fd584bb35518ee0f448c3
xuqil <xuqi19218@163.com>
2022-12-06 20:44:35 +0800

    orm: 编程方式自定义表名和列名

---
+++ orm/v5/select_test.go
@@ -0,0 +1,118 @@
+package orm
+
+import (
+	"database/sql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"leanring-go/orm/internal/errs"
+	"testing"
+)
+
+func TestSelector_Build(t *testing.T) {
+	db, err := NewDB()
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name    string
+		builder QueryBuilder
+
+		wantQuery *Query
+		wantErr   error
+	}{
+		{
+			name:    "select",
+			builder: NewSelector[TestModel](db),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model`;",
+				Args: nil,
+			},
+		},
+		{
+			name:    "from",
+			builder: NewSelector[TestModel](db).From("`test_model`"),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model`;",
+				Args: nil,
+			},
+		},
+		{
+			name:    "empty from",
+			builder: NewSelector[TestModel](db).From(""),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model`;",
+				Args: nil,
+			},
+		},
+		{
+			name:    "from db",
+			builder: NewSelector[TestModel](db).From("`test_db`.`test_model`"),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_db`.`test_model`;",
+				Args: nil,
+			},
+		},
+		{
+			name:    "empty where",
+			builder: NewSelector[TestModel](db).Where(),
+			wantQuery: &Query{
+				SQL: "SELECT * FROM `test_model`;",
+			},
+		},
+		{
+			name:    "where",
+			builder: NewSelector[TestModel](db).Where(C("Age").Eq(18)),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model` WHERE `age` = ?;",
+				Args: []any{18},
+			},
+		},
+		{
+			name:    "not",
+			builder: NewSelector[TestModel](db).Where(Not(C("Age").Eq(18))),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model` WHERE  NOT (`age` = ?);",
+				Args: []any{18},
+			},
+		},
+		{
+			name:    "and",
+			builder: NewSelector[TestModel](db).Where(C("Age").Eq(18).And(C("FirstName").Eq("Tom"))),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model` WHERE (`age` = ?) AND (`first_name` = ?);",
+				Args: []any{18, "Tom"},
+			},
+		},
+		{
+			name:    "or",
+			builder: NewSelector[TestModel](db).Where(C("Age").Eq(18).Or(C("FirstName").Eq("Tom"))),
+			wantQuery: &Query{
+				SQL:  "SELECT * FROM `test_model` WHERE (`age` = ?) OR (`first_name` = ?);",
+				Args: []any{18, "Tom"},
+			},
+		},
+		{
+			name:    "invalid column",
+			builder: NewSelector[TestModel](db).Where(C("Age").Eq(18).Or(C("XXXX").Eq("Tom"))),
+			wantErr: errs.NewErrUnknownField("XXXX"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			q, err := tc.builder.Build()
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantQuery, q)
+		})
+	}
+}
+
+type TestModel struct {
+	Id int64
+	// ""
+	FirstName string
+	Age       int8
+	LastName  *sql.NullString
+}
