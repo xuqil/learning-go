e444896df74580f63b41a5f0c3b760e5663156e7
xuqil <xuqi19218@163.com>
2023-09-05 20:57:07 +0800

    micro: 广播组播

--- micro/client.go
+++ micro/client.go
@@ -48,7 +48,7 @@ func ClientWithPickerBuilder(name string, b base.PickerBuilder) ClientOption {
 	}
 }
 
-func (c *Client) Dial(ctx context.Context, service string) (*grpc.ClientConn, error) {
+func (c *Client) Dial(ctx context.Context, service string, dialOptions ...grpc.DialOption) (*grpc.ClientConn, error) {
 	var opts []grpc.DialOption
 	if c.r != nil {
 		rb, err := NewRegistryBuilder(c.r, c.timeout)
@@ -64,6 +64,9 @@ func (c *Client) Dial(ctx context.Context, service string) (*grpc.ClientConn, er
 	}
 	if c.insecure {
 		opts = append(opts, grpc.WithInsecure())
+	}
+	if len(dialOptions) > 0 {
+		opts = append(opts, dialOptions...)
 	}
 	cc, err := grpc.DialContext(ctx, fmt.Sprintf("registry:///%s", service), opts...)
 	return cc, err
---
+++ micro/cluster/broadcast/broadcast.go
@@ -0,0 +1,61 @@
+package broadcast
+
+import (
+	"context"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"leanring-go/micro/registry"
+)
+
+type ClusterBuilder struct {
+	registry    registry.Registry
+	service     string
+	dialOptions []grpc.DialOption
+}
+
+func NewClusterBuilder(r registry.Registry, service string, dialOptions ...grpc.DialOption) *ClusterBuilder {
+	return &ClusterBuilder{
+		registry:    r,
+		service:     service,
+		dialOptions: dialOptions,
+	}
+}
+
+func (b ClusterBuilder) BuildUnaryInterceptor() grpc.UnaryClientInterceptor {
+	// method: users.UserService/GetByID
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		if !isBroadCast(ctx) {
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}
+		instances, err := b.registry.ListServices(ctx, b.service)
+		if err != nil {
+			return err
+		}
+		var eg errgroup.Group
+		for _, ins := range instances {
+			addr := ins.Address
+			eg.Go(func() error {
+				// 一个一个地调用
+				insCC, er := grpc.Dial(addr, b.dialOptions...)
+				if er != nil {
+					return er
+				}
+				return invoker(ctx, method, req, reply, insCC, opts...)
+			})
+		}
+		// 全部调用完毕
+		return eg.Wait()
+	}
+}
+
+func UseBroadCast(ctx context.Context) context.Context {
+	return context.WithValue(ctx, broadcastKey{}, true)
+}
+
+type broadcastKey struct {
+}
+
+func isBroadCast(ctx context.Context) bool {
+	val, ok := ctx.Value(broadcastKey{}).(bool)
+	return ok && val
+}
